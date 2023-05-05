9a6f1c869b7aed1dd06324fc1e29988c757d141b
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-05-05 17:39:01 -0400

    quote interruption v1

---
+++ modules/quote_inter.py
@@ -0,0 +1,70 @@
+import random
+import re
+
+def gen_quotes(n):
+    quotes = ['"', "'"]
+    string_tab = []
+    for i in range(n):
+        quote = random.choice(quotes)
+        string_tab.append(quote)
+        string_tab.append(quote)
+
+    return "".join(string_tab)
+
+def interrupt(payload):
+    """
+        Une fonction vraiment dégueulasse fait a la zeub a minuit si vous avez mieux pour mettre des quotes seulement entre les Cmdlet je suis preneur
+    """
+    fnl = ""
+    stop = False
+    stop_quote = False
+    crochet = 0
+    separator = [" ", ";", "'", '"', "`", "=", ","]
+    payload_divided_catch = payload.split("catch")
+    for x in range(len(payload_divided_catch)):
+        payload_divided_func = payload_divided_catch[x].split("function")
+        for y in range(len(payload_divided_func)):
+            payload_divided_try = payload_divided_func[y].split("try")
+            for z in range(len(payload_divided_try)):
+                payload_divided_while = payload_divided_try[z].split("while")
+                for a in range(len(payload_divided_while)):
+                    payload_list = list(payload_divided_while[a])
+                    payload_list.append("¨")
+                    i = 0
+
+                    while payload_list[i] != "¨":
+                        if payload_list[i] == "$" or payload_list[i] == "." or payload_list[i] == "-":
+                            stop = True
+                        elif payload_list[i] == "'" or payload_list[i] == '"':
+                            if stop_quote:
+                                stop_quote = False
+                            else:
+                                stop_quote = True
+                        elif payload_list[i] == "[" :
+                            crochet += 1
+                        elif payload_list[i] == "]":
+                            crochet -= 1            
+                        elif re.match("[0-9 ;'\"`=,]", payload_list[i] ) or payload_list[i] in separator:
+                            stop = False
+                        elif crochet == 0 and stop == False and stop_quote == False and random.choice([True, False]) and i != 0 and re.match("[a-zA-Z]", payload_list[i-1]) and re.match("[a-zA-Z]", payload_list[i]) and re.match("[a-zA-Z]", payload_list[i+1]):
+                            payload_list.insert(i, gen_quotes(1))
+                            i += 1
+                        i +=1
+                    payload_list.pop()
+                    fnl += "".join(payload_list)
+
+                    if a < len(payload_divided_while) -1:
+                        fnl += "while"
+
+                if z < len(payload_divided_try) -1:
+                    fnl += "try"
+
+            if y < len(payload_divided_func) -1:
+                fnl += "function"
+
+        if x < len(payload_divided_catch) -1:
+            fnl += "catch"
+    return fnl
+
+if __name__ == "__main__":
+    print(interrupt("mange tes.grand.mort[stp]; $cailloux = Test.dd; "))
--- obfuscator.py
+++ obfuscator.py
@@ -4,7 +4,7 @@ import re
 from modules.rename import rename as rename
 from modules.boolean import bool_edit as bool_edit
 from modules.entropy_calc import entropy as entropy_calc
-
+from modules.quote_inter import interrupt as quote_interrupt
 
 def get_file_content(path):
     f = open(path, 'r')
@@ -14,10 +14,10 @@ def get_file_content(path):
 
 
 def obfuscation(payload):
-    payload = rename(payload)
     if '$True' in payload or '$true' in payload or '$False' in payload or '$false' in payload:
         payload = bool_edit(payload)
-
+    payload = quote_interrupt(payload)
+    payload = rename(payload)
     print(payload)
     print(f'\nNew payload entropy: {entropy_calc(payload)}')
 
@@ -41,12 +41,19 @@ if __name__ == "__main__":
     p = get_file_content(args.file_name)
     if args.mode == None:
         obfuscation(p)
+
     elif re.match("[vV][aA][rR]", args.mode):
         payload = rename(p)
         print(payload)
+
     elif re.match("[bB][oO][oO][lL][eE][aA][nN]", args.mode):
         if '$True' in p or '$true' in p or '$False' in p or '$false' in p:
             payload = bool_edit(p)
         print(payload)
+
+    elif re.match("[qQ][uU][oO][tT][eE]", args.mode):
+        payload = quote_interrupt(p)
+        print(payload)
+
     elif re.match("[eE][nN][tT][rR][oO][pP][yY]", args.mode):
         print(entropy_calc(p))
