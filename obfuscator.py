46220c4a8222507e11444d9d5f889290cb8e6b11
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-05-05 13:38:07 -0400

    set mode

--- obfuscator.py
+++ obfuscator.py
@@ -1,22 +1,52 @@
-from sys import argv
+import argparse
+import re
+
 from modules.rename import rename as rename
 from modules.boolean import bool_edit as bool_edit
 from modules.entropy_calc import entropy as entropy_calc
 
+
 def get_file_content(path):
-	f = open(path, 'r')
-	content = f.read()
-	f.close()
-	return content
-	
+    f = open(path, 'r')
+    content = f.read()
+    f.close()
+    return content
+
+
 def obfuscation(payload):
     payload = rename(payload)
     if '$True' in payload or '$true' in payload or '$False' in payload or '$false' in payload:
         payload = bool_edit(payload)
-    
+
     print(payload)
     print(f'\nNew payload entropy: {entropy_calc(payload)}')
 
+
 if __name__ == "__main__":
-    p = get_file_content(argv[1])
-    obfuscation(p)
+    
+    parser = argparse.ArgumentParser(description="print hello")
+    parser.add_argument(
+        "-f",
+        "--file_name",
+        type=str,
+        help="Enter the file name"
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        help="Enter the mode"
+    )
+    args = parser.parse_args()
+    p = get_file_content(args.file_name)
+    if args.mode == None:
+        obfuscation(p)
+    elif re.match("[vV][aA][rR]", args.mode):
+        payload = rename(p)
+        print(payload)
+    elif re.match("[bB][oO][oO][lL][eE][aA][nN]", args.mode):
+        if '$True' in p or '$true' in p or '$False' in p or '$false' in p:
+            payload = bool_edit(p)
+        print(payload)
+    elif re.match("[eE][nN][tT][rR][oO][pP][yY]", args.mode):
+        print(entropy_calc(p))
